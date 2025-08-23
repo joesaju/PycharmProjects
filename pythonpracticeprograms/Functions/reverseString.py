d7193a6d8f817a456c5276cfbe2d5c5c5919a95f
joesaju <saju.x19@gmail.com>
2025-08-23 06:01:46 +0530

    git commit -m "reverseString pgm committed"

--- pythonpracticeprograms/Functions/reverseString.py
+++ pythonpracticeprograms/Functions/reverseString.py
@@ -1,6 +1,10 @@
-str = input("enter the string:")
-reversed=""
-for char in str:
-    reversed = char + reversed
-print("reversed string is:",reversed)
+def reversed_str(str):
+    reversed=""
+    for char in str:
+        reversed = char + reversed
+    print("reversed string is:",reversed)
+    return reversed
+
+str=input("enter the string:")
+reversed_str(str)
    
