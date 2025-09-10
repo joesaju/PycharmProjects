b28b76612ff9bb422c6b00fdaecc8a69ed7698b1
joesaju <saju.x19@gmail.com>
2025-09-10 14:55:47 +0530

    commit -m "committed app.py"

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -8,9 +8,9 @@ with open(r"D:\python\New folder\Streamlit\classifier.pkl", "rb") as model_file:
 st.title("Revenue Predictor")
 st.write("Enter the following details:")
 
-Page_Views = st.slider("Views", min_value=50.0, max_value=500.0)
-Clicks = st.slider("Clicks", min_value=10.0, max_value=100.0)
-Signups = st.slider("Signups", min_value=1.0, max_value=20.0)
+Page_Views = st.slider("Views", min_value=50, max_value=500)
+Clicks = st.slider("Clicks", min_value=10, max_value=100)
+Signups = st.slider("Signups", min_value=1, max_value=200)
 
 if st.button("Predict"):
     features = np.array([[Page_Views, Clicks, Signups]])
