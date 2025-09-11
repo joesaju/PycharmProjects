f834417d516b65ae12dc0e0c11d2d3eafa4a454d
joesaju <saju.x19@gmail.com>
2025-09-11 10:46:02 +0530

    commit -m "committed data"

--- Streamlit/streamlit_revenue.py
+++ Streamlit/streamlit_revenue.py
@@ -30,14 +30,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # ---- Model Training with tuned parameters ----
-model = RandomForestRegressor(
-    n_estimators=300,      # number of trees
-    max_depth=15,          # control overfitting
-    min_samples_split=5,
-    min_samples_leaf=2,
-    random_state=42,
-    n_jobs=-1              # use all CPU cores
-)
+model = RandomForestRegressor()
 
 model.fit(X_train, y_train)
 
