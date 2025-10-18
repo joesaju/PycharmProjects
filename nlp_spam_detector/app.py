6840765e1131e72be19f3c937a6f0caed4b3f33e
joesaju <saju.x19@gmail.com>
2025-10-18 23:10:31 +0530

    commited changes in app.py

--- nlp_spam_detector/app.py
+++ nlp_spam_detector/app.py
@@ -5,11 +5,15 @@ import os
 # Get the absolute path of the current file (app.py)
 current_dir = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(current_dir, "spam_model.pkl")
-vector_path = os.path.join(current_dir,"vectorizer.pkl")
-# Now safely open
-with open(model_path,vector_path, "rb") as f:
+vector_path = os.path.join(current_dir, "vectorizer.pkl")
+
+# Load the model
+with open(model_path, "rb") as f:
     model = pickle.load(f)
 
+# Load the vectorizer
+with open(vector_path, "rb") as f:
+    vectorizer = pickle.load(f)
 # Load saved model and vectorizer
 # model = pickle.load(open('models/spam_model.pkl', 'rb'))
 # vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
@@ -18,6 +22,6 @@ with open(model_path,vector_path, "rb") as f:
 st.title("SMS Spam Detector")
 input_msg = st.text_area("Enter a message:")
 if st.button("Predict"):
-    msg_vec = vectorizer.transform([input_msg])
+    msg_vec = vector_path.transform([input_msg])
     prediction = model.predict(msg_vec)
     st.write("Result:", "🚫 Spam" if prediction[0] == 1 else "✅ Not Spam")
