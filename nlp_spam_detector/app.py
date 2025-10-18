6a67fab122721764cdf549dcaa8863839ce6f60b
joesaju <saju.x19@gmail.com>
2025-10-18 23:20:04 +0530

    commit changes in app.py

--- nlp_spam_detector/app.py
+++ nlp_spam_detector/app.py
@@ -1,27 +1,23 @@
 import streamlit as st
-import pickle
-import os
+import pickle, os
 
-# Get the absolute path of the current file (app.py)
-current_dir = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(current_dir, "spam_model.pkl")
-vector_path = os.path.join(current_dir, "vectorizer.pkl")
+# Cache the model and vectorizer so they load only once
+@st.cache_resource
+def load_model_and_vectorizer():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(current_dir, r"D:\python\New folder\nlp_spam_detector\models\spam_model.pkl"), "rb") as f:
+        model = pickle.load(f)
+    with open(os.path.join(current_dir, r"D:\python\New folder\nlp_spam_detector\models\vectorizer.pkl"), "rb") as f:
+        vectorizer = pickle.load(f)
+    return model, vectorizer
 
-# Load the model
-with open(model_path, "rb") as f:
-    model = pickle.load(f)
+model, vectorizer = load_model_and_vectorizer()
 
-# Load the vectorizer
-with open(vector_path, "rb") as f:
-    vectorizer = pickle.load(f)
-# Load saved model and vectorizer
-# model = pickle.load(open('models/spam_model.pkl', 'rb'))
-# vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
+st.title("📧 NLP Spam Detector")
 
+input_msg = st.text_input("Enter your message")
 
-st.title("SMS Spam Detector")
-input_msg = st.text_area("Enter a message:")
 if st.button("Predict"):
-    msg_vec = vector_path.transform([input_msg])
-    prediction = model.predict(msg_vec)
-    st.write("Result:", "🚫 Spam" if prediction[0] == 1 else "✅ Not Spam")
+    msg_vec = vectorizer.transform([input_msg])
+    result = model.predict(msg_vec)[0]
+    st.success("✅ Not Spam" if result == 0 else "🚨 Spam Message Detected")
