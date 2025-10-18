793813b8d70bddce08ff32f3763e51cd4af9c11f
joesaju <saju.x19@gmail.com>
2025-10-18 23:06:26 +0530

    commit changes in app.py

--- nlp_spam_detector/app.py
+++ nlp_spam_detector/app.py
@@ -1,8 +1,18 @@
 import streamlit as st
 import pickle
+import os
+
+# Get the absolute path of the current file (app.py)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(current_dir, "spam_model.pkl")
+vector_path = os.path.join(current_dir,"vectorizer.pkl")
+# Now safely open
+with open(model_path,vector_path, "rb") as f:
+    model = pickle.load(f)
+
 # Load saved model and vectorizer
-model = pickle.load(open('models/spam_model.pkl', 'rb'))
-vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
+# model = pickle.load(open('models/spam_model.pkl', 'rb'))
+# vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
 
 
 st.title("SMS Spam Detector")
